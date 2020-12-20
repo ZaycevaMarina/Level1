560cb4a0199b0dcc3edf946d1cccbc981e193ea0
ZaycevaMarina <mari_minflash@yahoo.com>
2020-12-20 20:46:57 +0300

    Урок 1. Задания 1- 6.

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,41 @@
+﻿#region 
+//Зайцева Марина
+//Урок 1. Задание 1
+//Написать программу "Анкета". Последовательно задаются вопросы (имя, фамилия, возраст, рост, вес).
+//В результате информация выводится в одну строчку:
+//  а) используя склеивание;
+//  б) используя форматированный вывод;
+//  в) используя вывод со знаком $
+#endregion
+
+using System;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, surname, age, height, weight;
+            Console.WriteLine("Программа \"Анкета\"");
+            Console.WriteLine("Введите Ваше имя:");
+            name = Console.ReadLine();
+            Console.WriteLine("Введите фамилию:");
+            surname = Console.ReadLine();
+            Console.WriteLine("Введите возраст:");
+            age = Console.ReadLine();
+            Console.WriteLine("Введите рост:");
+            height = Console.ReadLine();
+            Console.WriteLine("Введите вес:");
+            weight = Console.ReadLine();
+            //a)
+            Console.WriteLine("Ваше имя: " + name + ", фамилия " + surname + ", возраст " + age + ", рост " + height + ", вес " + weight);
+            //б)
+            Console.WriteLine("Ваше имя: {0}, фамилия {1}, возраст {2}, рост {3}, вес {4}", name, surname, age, height, weight);
+            //в)
+            Console.WriteLine($"Ваше имя: {name}, фамилия {surname}, возраст {age}, рост {height}, вес {weight}");
+
+          
+        }
+    }
+}
---
+++ Lesson_1_5/Program.cs
@@ -0,0 +1,28 @@
+﻿#region
+/*Зайцева Марина
+ * Урок 1. Задание 5
+ * а) Вывести на экран имя, фамилию и город проживания
+ * б) Организовать в центре экрана
+ * в) использовать собственный метод
+ */
+#endregion
+using System;
+
+namespace Lesson_1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 40, y = 13; //Примерно в центре окна
+            Print("(с) Марина Зайцева, г.Чехов", x, y);
+            Console.ReadKey();            
+        }
+
+        private static void Print(string msg, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(msg);
+        }
+    }
+}
